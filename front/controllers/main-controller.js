62f1b29d722c26cc1e607b9bfd06e3fecd0f86cb
Seth Sutherland <cokolatepal@gmail.com>
2017-05-22 10:50:08 -0600

    commit

--- front/controllers/main-controller.js
+++ front/controllers/main-controller.js
@@ -17,15 +17,37 @@ angular.module('myApp').controller('mainCtrl', function($scope, mainService){
     //     return a - b;
     // });
 
-    d3.select(".main-container")
+    d3.select(".bar-container")
         .selectAll("div")
         .data(dataset)
         .enter()
         .append("div")
         .attr("class", "bar")
         .style("height", function(d){
-            var barHeight = d * 5;
+            var barHeight = d * 5 + 10;
             return barHeight + "px";
         })
+        .append("p")
+        .text(function(d){
+            return d;
+        });
+
+    var circles = d3.select(".circle-container")
+        .selectAll("div")
+        .data(dataset)
+        .enter()
+        .append("div")
+        .attr("class", "circle")
+        .style("height", function(d){
+            var circleHeight = d * 2;
+            return circleHeight + "px";
+        }).text(function(d){
+            return d
+        });
+
+    circles.style("width", function(d){
+        return d * 2 + "px";
+    })
+
 
 });
