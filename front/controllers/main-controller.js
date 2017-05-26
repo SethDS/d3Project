b8435a03496369319c61176cd8428489faa2a0c3
Seth Sutherland <cokolatepal@gmail.com>
2017-05-25 19:44:22 -0600

    finished

--- front/controllers/main-controller.js
+++ front/controllers/main-controller.js
@@ -3,51 +3,238 @@
  */
 angular.module('myApp').controller('mainCtrl', function($scope, mainService){
 
-    var dataset = [];                        //Initialize empty array
+    var data = mainService.data,
+        totalPopPer = [
+            {
+                num: .502,
+                gender: "m"
+            },
+            {
+                num: .498,
+                gender: "f"
+            }
+        ],
+        maleData = data[1].maleBreakDown,
+        femaleData = data[1].femaleBreakDown,
+        dummyData = [1],
 
-    function cool() {
-        for (var i = 0; i < 25; i++) {           //Loop 25 times
-            var newNumber = Math.round(Math.random() * 30);
-            dataset.push(newNumber);             //Add new number to array
-        }
-    }
+        popData = function(dat){
 
-    setTimeout(cool(), 5000);
-    // dataset.sort(function(a,b){
-    //     return a - b;
-    // });
+                var dataCont = d3.select(".pop-data"),
+                     h = document.getElementsByClassName("pop-data")[0].clientHeight,
+                    w = document.getElementsByClassName("pop-data")[0].clientWidth,
+                    svg = dataCont
+                        .append("svg")
+                        .attr("width", w)
+                        .attr("height", h);
 
-    d3.select(".bar-container")
-        .selectAll("div")
-        .data(dataset)
-        .enter()
-        .append("div")
-        .attr("class", "bar")
-        .style("height", function(d){
-            var barHeight = d * 5 + 10;
-            return barHeight + "px";
-        })
-        .append("p")
-        .text(function(d){
-            return d;
-        });
 
-    var circles = d3.select(".circle-container")
-        .selectAll("div")
-        .data(dataset)
-        .enter()
-        .append("div")
-        .attr("class", "circle")
-        .style("height", function(d){
-            var circleHeight = d * 2;
-            return circleHeight + "px";
-        }).text(function(d){
-            return d
-        });
+                    svg.selectAll("rect")
+                        .data(dat)
+                        .enter()
+                        .append("rect")
+                        .attr("x", 0)
+                        .attr("y", function (d,i) {
+                            if(d.gender === "m"){
+                                return 0
+                            } else {
+                                return "40%"
+                            }
+                        })
+                        .attr("width", function(d){
+                            return w * d.num;
+                        })
+                        .attr("height", h / 3)
+                        .attr("fill", function(d,i){
+                            if(d.gender === "m"){
+                                return "red"
+                            } else {
+                                return "darkred"
+                            }
+                        });
 
-    circles.style("width", function(d){
-        return d * 2 + "px";
-    })
+            svg.selectAll("rectOpp")
+                .data(dat)
+                .enter()
+                .append("rect")
+                .attr("x", function(d){
+                    return d.num * w;
+                })
+                .attr("y", function (d,i) {
+                    if(d.gender === "m"){
+                        return 0
+                    } else {
+                        return "40%"
+                    }
+                })
+                .attr("width", function(d){
+                    return w * (1 - d.num);
+                })
+                .attr("height", h / 3)
+                .attr("fill", function(d,i){
+                    if(d.gender === "m"){
+                        return "red"
+                    } else {
+                        return "darkred"
+                    }
+                })
+                .attr("opacity", "0.2");
+
+                        svg.selectAll("text")
+                            .data(dat)
+                            .enter()
+                            .append("text")
+                            .text(function(d){
+                            if(d.gender === "m"){
+                                        return "Men make up 50.2%";
+                                    } else {
+                                        return "Women make up 49.8%";
+                                    }
+                                })
+                            .attr("x", "20%")
+                            .attr("y",function (d) {
+                                if(d.gender === "m"){
+                                    return "20%"
+                                } else {
+                                    return "60%"
+                                }
+                            })
+                            .attr("text-anchor", "middle")
+                            .attr("font-family", "Julius Sans One")
+                            .attr("font-size", "medium")
+                            .attr("font-weight", "bold")
+                            .attr("fill", "black");
+
+    };
+
+    var malePop = function(dat){
+
+        var dataContainer = d3.select(".male-data-container"),
+            h = document.getElementsByClassName("male-data-container")[0].clientHeight,
+            w = document.getElementsByClassName("male-data-container")[0].clientWidth,
+            svg = dataContainer
+                .append("svg")
+                .attr("width", w)
+                .attr("height", h);
+
+            svg.selectAll("rect")
+                .data(dat)
+                .enter()
+                .append("rect")
+                .attr("x", function(d,i){
+                    return i * (w / (dat.length));
+                })
+                .attr("y", 0)
+                .attr("height", function(d){
+                    return h * (d.percentage * 0.01);
+                })
+                .attr("width", (w - 100) / dat.length + 5)
+                .attr("fill", "red");
+
+
+        svg.selectAll("rectOpp")
+            .data(dat)
+            .enter()
+            .append("rect")
+            .attr("x", function(d,i){
+                return i * (w / (dat.length));
+            })
+            .attr("y", function(d){
+                return h * (d.percentage * 0.01);
+            })
+            .attr("height", function(d){
+                return h * (1 - (d.percentage * 0.01));
+            })
+            .attr("width", (w - 100) / dat.length + 5)
+            .attr("fill", "red")
+            .attr("opacity", "0.3");
+
+
+        svg.selectAll("text")
+            .data(dat)
+            .enter()
+            .append("text")
+            .text(function(d){
+                return "Males ages " + d.ageGroup + " comprise " + d.percentage + "% of the population";
+            })
+            .attr("x", h - (h * 1.5))
+            .attr("y", function(d, i) {
+                return  (w / dat.length) * i + 70;
+            })
+            .attr("text-anchor", "middle")
+            .attr("class", "male-pop-text")
+            .attr("font-family", "Julius Sans One")
+            .attr("font-size", "large")
+            .attr("fill", "white")
+            .attr("class", "male-data-text");
+
+    };
+
+    var femalePop = function(dat){
+
+        var dataContainer = d3.select(".female-data-container"),
+            h = document.getElementsByClassName("female-data-container")[0].clientHeight,
+            w = document.getElementsByClassName("female-data-container")[0].clientWidth,
+            svg = dataContainer
+                .append("svg")
+                .attr("width", w)
+                .attr("height", h);
+
+        svg.selectAll("rect")
+            .data(dat)
+            .enter()
+            .append("rect")
+            .attr("x", function(d,i){
+                return i * (w / (dat.length));
+            })
+            .attr("y", 0)
+            .attr("height", function(d){
+                return h * (d.percentage * 0.01);
+            })
+            .attr("width", (w - 100) / dat.length + 5)
+            .attr("fill", "darkred");
+
+        svg.selectAll("rectOpp")
+            .data(dat)
+            .enter()
+            .append("rect")
+            .attr("x", function(d,i){
+                return i * (w / (dat.length));
+            })
+            .attr("y", function(d){
+                return h * (d.percentage * 0.01);
+            })
+            .attr("height", function(d){
+                return h * (1 - (d.percentage * 0.01));
+            })
+            .attr("width", (w - 100) / dat.length + 5)
+            .attr("fill", "red")
+            .attr("opacity", "0.2");
+
+        svg.selectAll("text")
+            .data(dat)
+            .enter()
+            .append("text")
+            .text(function(d){
+                return "Males ages " + d.ageGroup + " comprise " + d.percentage + "% of the population";
+            })
+            .attr("x", h - (h * 1.5))
+            .attr("y", function(d, i) {
+                return  (w / dat.length) * i + 70;
+            })
+            .attr("text-anchor", "middle")
+            .attr("class", "male-pop-text")
+            .attr("font-family", "Julius Sans One")
+            .attr("font-size", "large")
+            .attr("fill", "white")
+            .attr("class", "male-data-text");
+
+    };
+
+
+    popData(totalPopPer);
+    malePop(maleData);
+    femalePop(femaleData);
 
 
 });
--- front/services/main-service.js
+++ front/services/main-service.js
@@ -3,5 +3,72 @@
  */
 angular.module('myApp').service('mainService', function() {
 
+this.data = [
+    {
+        section: 'Population',
+        totalPop: 79801
+    },
+    {
+        section: 'Population Breakdown',
+        male: 40061,
+        female: 39741,
+        maleBreakDown: [
+            {
+                ageGroup: "0 to 4",
+                pop: 7820,
+                percentage: 9.8
+            },
+            {
+                ageGroup: "5 to 11",
+                pop: 9097,
+                percentage: 11.4
+            },
+            {
+                ageGroup: "12 to 17",
+                pop: 5905,
+                percentage: 7.4
+            },
+            {
+                ageGroup: "18 to 59",
+                pop: 16359,
+                percentage: 20.5
+            },
+            {
+                ageGroup: "60 plus",
+                pop: 878,
+                percentage: 1.1
+            }
+        ],
+        femaleBreakDown:[
+            {
+                ageGroup: "0 to 4",
+                pop: 7820,
+                percentage: 9.2
+            },
+            {
+                ageGroup: "5 to 11",
+                pop: 9097,
+                percentage: 10.8
+            },
+            {
+                ageGroup: "12 to 17",
+                pop: 5905,
+                percentage: 7.1
+            },
+            {
+                ageGroup: "18 to 59",
+                pop: 16359,
+                percentage: 21.1
+            },
+            {
+                ageGroup: "60 plus",
+                pop: 878,
+                percentage: 1.6
+            }
+        ]
+    }
+];
+
+console.log("what the crap is going on??");
 
 });
